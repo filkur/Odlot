c1749cab26ac129c8461e839de066df9e4e25a79
filkur <filipkuranda@gmail.com>
2020-12-21 17:15:24 +0100

    Add navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,93 +8,75 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+            .container-fluid{
+                font-family: 'Noto Serif', serif;
+                max-width: 1400px;
             }
-
-            .full-height {
-                height: 100vh;
+            .navbar{
+                font-size: 25px;
             }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .btn-outline-secondary{
+                margin-left: 20px;
+                border-radius: 40px;
             }
         </style>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+        <div class="container-fluid" >
+            <header>
+                <nav class="navbar col-sm-border">
+                    <div class="container-fluid">
+                            <a class="navbar-brand" href="#">
+                                <img src="{{ asset('img/logo.JPG') }}" alt="" width="450" height="220" class="d-inline-block align-top">
+                            </a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                        <ul class="nav justify-content-end">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Aktualności</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Kontakt</a>
+                            </li>
+                            <li>
+                                <button class="btn btn-sm btn-outline-secondary" type="button" style="background-color: #636b6f; color: #f8fafc; border-radius: 20px; font-size: 23px;" >Login</button>
+                            </li>
+                            <li>
+                                <button class="btn btn-sm btn-outline-secondary" type="button" style="background-color: rgba(44,81,255,0.76); color: #f8fafc; border-radius: 20px;  font-size: 23px;">Rejestracja</button>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <div class="container">
+                <h2>Lorem ipsum</h2>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ultrices justo et eleifend laoreet. Pellentesque sit amet pulvinar ipsum. Etiam vitae lacinia tellus. Curabitur tincidunt, felis eu finibus consequat, justo lacus mattis dolor, sed iaculis odio arcu nec velit. Curabitur egestas, tellus at volutpat elementum, urna tortor porta neque, sed lobortis orci purus id est. Integer condimentum, erat quis lobortis pulvinar, ante tellus fermentum erat, vitae fermentum risus mauris vitae elit. Duis eu sapien purus. Morbi nec consectetur eros. Maecenas nec aliquet magna. Aliquam ipsum tortor, volutpat at risus ac, fermentum ultrices ligula. Nam blandit, massa id vulputate ornare, arcu est ornare tellus, et sollicitudin leo elit non neque.
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                Duis tristique massa magna, accumsan tristique dui viverra at. Ut dictum tellus et metus scelerisque, vitae condimentum odio sodales. Phasellus molestie ante a tincidunt commodo. Donec vel dictum leo. Nunc non sem orci. Curabitur mollis massa sit amet nibh scelerisque scelerisque. Curabitur iaculis porta porta.
             </div>
         </div>
     </body>
 </html>
+
+<!-- <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+    <div class="top-right links">
+@auth
+        <a href="{{ url('/home') }}">Home</a>
+                    @else
+        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+            <a href="{{ route('register') }}">Register</a>
+                        @endif
+    @endauth
+        </div>
+@endif
+    -->
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/o-nas', function (){
+    return "o nas";
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
