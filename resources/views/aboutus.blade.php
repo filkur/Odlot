8d85a6eee47083da0d3a58197fa4ba027b287481
filkur <filipkuranda@gmail.com>
2020-12-28 18:11:09 +0100

    modify page title

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Odlot.pl/o-nas</title>
+    <title>Odlot.pl</title>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
