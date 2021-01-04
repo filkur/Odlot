8846c61e83c9afe579ea50d1a2587c31d4cdd5aa
filkur <filipkuranda@gmail.com>
2021-01-04 10:48:33 +0100

    create history ticket page

--- resources/views/bookticket.blade.php
+++ resources/views/bookticket.blade.php
@@ -4,7 +4,10 @@
     <div class="container">
         <div class="card">
             <div class="card-header"><h3>Gratulacje! Zakupiono bilet</h3></div>
-            <div class="card-body">Nie czekaj, sprawdź w swojej historii czy już on jest!</div>
+            <div class="card-body">
+                <p>Nie czekaj kliknij w przycisk poniżej i sprawdź w swojej historii czy już on jest!</p>
+                <a href="{{route('#')}}" class="btn btn-primary btn-sm">Sprawdź</a>
+            </div>
         </div>
     </div>
 
---
+++ resources/views/history.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header"><h3>Zakupione bilety:</h3></div>
+            <div class="card-body">
+                <div class="row">
+                    Od Chichago po Wodzisław na prezesa tylko zdzisław
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
